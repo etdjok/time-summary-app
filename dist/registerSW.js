24c4784ae6227a82a588cbb269db81f1f6b6b262
你的名字 <wxpemail@163.com>
2026-08-06 11:39:22 +0800

    v1.18.5: 强制清除SW缓存 - 添加版本号参数和no-cache meta

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js?v=18f', { scope: './' }).then(reg => {if(reg.waiting) reg.waiting.postMessage({type:'SKIP_WAITING'})})})}
